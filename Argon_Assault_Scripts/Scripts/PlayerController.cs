bbc230700fdbb5af1a4f123cb45cabf8f41d8d2d
Ivan Franco <ifrancoh@ucsc.edu>
2024-06-29 00:41:01 -0700

    added player movement to player ship (learned about pitch, yaw and roll rotation)

--- Argon_Assault_Scripts/Scripts/PlayerController.cs
+++ Argon_Assault_Scripts/Scripts/PlayerController.cs
@@ -1,39 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using UnityEngine.InputSystem;
+// using UnityEngine.InputSystem; for new input system
 
 public class PlayerController : MonoBehaviour
 {
     // [SerializeField] InputAction movement;
+    [SerializeField] private float controlSpeed = 15f;
+    [SerializeField] private float xRange = 2f;
+    [SerializeField] private float yRange = 3f;
+    [SerializeField] private float positionPitchFactor = -8f;
+    [SerializeField] private float controlPitchFactor = -20f;
+    [SerializeField] private float positionYawFactor = 10f;
+    [SerializeField] private float controlRollFactor = -40f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    /*
-    private void OnEnable()
-    {
-        movement.Enable();
-    }
-
-    rivate void OnDisable()
-    {
-        movement.Disable();
-    }
-    */
+    private float xThrow;
+    private float yThrow;
 
     // Update is called once per frame
     void Update()
     {
-        // float horizontalThrow = movement.ReadValue<Vector3>().x;
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
-
+        // New Input System
+        // float horizontalThrow = movement.ReadValue<Vector2>().x;
         // float verticalThrow = movement.ReadValue<Vector2>().y;
-        float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        // get value from keyboard input
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
+        // changing x position on Player Ship (using localPosition)
+        float xOffSet = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffSet;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        // changing y position on Player Ship
+        float yOffSet = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffSet;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
+/*
+  // have to enable new input system when game starts
+  private void OnEnable()
+  {
+      movement.Enable();
+  }
+
+  // have to disable new input system when game closes
+  private void OnDisable()
+  {
+      movement.Disable();
+  }
+  */
